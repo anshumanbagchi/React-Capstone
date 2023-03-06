ff9fa900115d72c561159662828c239653b014ce
Anshuman Bagchi <bagchia@github>
2023-03-06 20:41:23 +0530

    Added Cart context, Cart icon, Cart dropdown

--- react-capstone/src/components/routes/navigation/navigation.route.component.jsx
+++ react-capstone/src/components/routes/navigation/navigation.route.component.jsx
@@ -4,11 +4,16 @@ import { ReactComponent as CrwnLogo } from "../../../assets/crown.svg";
 import { Outlet, Link } from "react-router-dom";
 import { signOutUser } from "../../../utils/firebase/firebase.utils";
 import { UserContext } from "../../../contexts/user.context";
+import { CartContext } from "../../../contexts/cart.context";
+
+import CartIcon from "../../cart-icon/cart-icon.component";
 
 import "./navigation.styles.scss";
+import CartDropdown from "../../cart-dropdown/cart-dropdown.component";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
@@ -29,7 +34,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
