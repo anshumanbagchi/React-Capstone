c453d03d515fbb1babcae797f3fff1e78c3ec0f8
Anshuman Bagchi <bagchia@github>
2023-03-06 20:04:43 +0530

    Created Product Context, Shop page, Product Card

--- react-capstone/src/App.js
+++ react-capstone/src/App.js
@@ -3,13 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/routes/home/home.route.component";
 import Navigation from "./components/routes/navigation/navigation.route.component";
 import Authentication from "./components/routes/auth/auth.route.component";
+import Shop from "./components/routes/shop/shop.route.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        {/* <Route path="shop" element={<Shop />} /> */}
+        <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
