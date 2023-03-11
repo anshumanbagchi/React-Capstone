c1223ed78efd069ed33b158d89a03861f7afc400
Anshuman Bagchi <bagchia@github>
2023-03-12 02:43:08 +0530

    Added redux-thunk, loading spinner

--- react-capstone/src/components/routes/shop/shop.route.component.jsx
+++ react-capstone/src/components/routes/shop/shop.route.component.jsx
@@ -4,19 +4,14 @@ import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.route.component";
 import Category from "../category/category.route.component";
-import { getCategoriesAndDocs } from "../../../utils/firebase/firebase.utils";
-import { setCategories } from "../../../store/categories/category.actions";
+import { fetchCategoriesAsync } from "../../../store/categories/category.actions";
 
 // import "./shop.styles.jsx";
 
 const Shop = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocs();
-      dispatch(setCategories(categoriesArray));
-    };
-    getCategoriesMap();
+    dispatch(fetchCategoriesAsync());
   }, []);
   return (
     <Routes>
--- react-capstone/src/store/categories/category.selector.js
+++ react-capstone/src/store/categories/category.selector.js
@@ -10,3 +10,5 @@ export const selectCategoriesMap = createSelector([selectCategories], (categorie
     return acc;
   }, {})
 );
+
+export const selectCategoriesIsLoading = createSelector([selectCategoryReducer], (categoriesSlice) => categoriesSlice.isLoading);
